b83be2af6546fac325a9fd72c4ac843521fd75be
Danylo Kazmin <danylo.kazmin@nixs.com>
2025-04-10 18:59:05 +0200

    updated port

--- Blog in Blazor/Blog.BlazorServer/Program.cs
+++ Blog in Blazor/Blog.BlazorServer/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddScoped(sp =>
 
     var httpClient = new HttpClient(authHeaderHandler)
     {
-        BaseAddress = new Uri("https://localhost:62755")
+        BaseAddress = new Uri("https://localhost:49940")
     };
 
     return httpClient;
